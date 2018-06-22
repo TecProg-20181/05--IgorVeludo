a561e7ffb84bc24015d88f672ef3b75fa2b1d93d
IgorVeludo <velvet.guimaraes@gmail.com>
2018-06-22 17:10:30 -0300

    Creating test for check_output method

--- diskspace/test.py
+++ diskspace/test.py
@@ -1,4 +1,5 @@
 import unittest
+import subprocess
 from diskspace import *
 
 
@@ -10,6 +11,11 @@ class Test(unittest.TestCase):
         command = bytes_to_readable(blocks)
         self.assertEqual(command, result)
 
+    def testing_subprocess_check_output(self):
+        command = 'du'
+        firstResult = subprocess.check_output(command.strip().split(' '))
+        finalResult = subprocess_check_output(command)
+        self.assertEqual(firstResult, finalResult)
 
 if __name__ == '__main__':
     unittest.main()
