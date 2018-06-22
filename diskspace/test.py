8915af31333b349c0540c893ae96022e150df7b4
IgorVeludo <velvet.guimaraes@gmail.com>
2018-06-22 17:34:31 -0300

    Creating test to show_space_list method

--- diskspace/test.py
+++ diskspace/test.py
@@ -5,17 +5,21 @@ from diskspace import *
 
 class Test(unittest.TestCase):
 
-    def testing_bytes_to_readble(self):
+    def test_bytes_to_readble(self):
         blocks = 224
-        result = "112.00Kb"
         command = bytes_to_readable(blocks)
-        self.assertEqual(command, result)
+        self.assertEqual(command, "112.00Kb")
 
-    def testing_subprocess_check_output(self):
+
+
+    def test_subprocess_check_output(self):
         command = 'du'
         firstResult = subprocess.check_output(command.strip().split(' '))
         finalResult = subprocess_check_output(command)
         self.assertEqual(firstResult, finalResult)
+
+    def test_show_space_list(self):
+        self.assertIsNone(show_space_list(directory='.', depth=-1, order=True))
 
 if __name__ == '__main__':
     unittest.main()
