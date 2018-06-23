f85879c39d9d7637936622bb7463d9dd2c0b02d0
IgorVeludo <velvet.guimaraes@gmail.com>
2018-06-22 21:30:00 -0300

    Anothers test for bytes method

--- diskspace/test.py
+++ diskspace/test.py
@@ -13,6 +13,18 @@ class Test(unittest.TestCase):
         function = bytes_to_readable(1024)
         self.assertEquals('512.00Kb', function)
 
+    def test_bytes_to_readable_Mb(self):
+        function = bytes_to_readable(1048576)
+        self.assertEquals('512.00Mb', function)
+
+    def test_bytes_to_readable_Gb(self):
+        function = bytes_to_readable(1073750000)
+        self.assertEquals('512.00Gb', function)
+
+    def test_bytes_to_readable_Tb(self):
+        function = bytes_to_readable(1099504999000)
+        self.assertEquals('512.00Tb', function)
+
     def test_subprocess_check_output(self):
         command = 'du'
         firstResult = subprocess.check_output(command.strip().split(' '))
